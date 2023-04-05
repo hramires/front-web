b49e58ebd6a39b69c018257e1b8dafa939df3187
Douglas <dodosobreira@gmail.com>
2023-04-05 19:01:33 -0300

    add new style, and init gallery script

--- src/views/registries/register-enterprise.vue
+++ src/views/registries/register-enterprise.vue
@@ -1,55 +1,109 @@
 <script>
-import { BCard, BForm, BFormGroup, BFormInput, BButton } from "bootstrap-vue";
+import {
+  BForm, BFormGroup, BFormInput, BButton,
+} from 'bootstrap-vue';
 
 export default {
   page: {
-    meta: [{ name: "description", content: "Sign In" }]
+    meta: [{
+      name: 'description',
+      content: 'Sign In',
+    }],
   },
   components: {
-    BCard,
     BForm,
     BFormGroup,
     BFormInput,
-    BButton
+    BButton,
   },
   methods: {
     onSubmit() {
-      console.log("submit");
+      console.log('submit');
       alert(JSON.stringify(this.form));
     },
     goBack() {
-      this.$router.push({ name: "home" });
-    }
+      this.$router.push({ name: 'home' });
+    },
   },
   data() {
     return {
       form: {
-        name: "",
-        description: "",
-        contact: "",
-        schedule: "",
-        categories: []
+        name: '',
+        description: '',
+        contact: '',
+        schedule: '',
+        categories: [],
       },
       items: [
-        { id: "1", categoria: "Exemplo Categoria" },
-        { id: "2", categoria: "Exemplo Categoria 2" }
+        {
+          id: '1',
+          categoria: 'Exemplo Categoria',
+        },
+        {
+          id: '2',
+          categoria: 'Exemplo Categoria 2',
+        },
       ],
-      show: true
+      show: true,
+      images: [
+        {
+          blank: true,
+          blankColor: '#000',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+        {
+          blank: true,
+          blankColor: '#777',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+        {
+          blank: true,
+          blankColor: 'blue',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+        {
+          blank: true,
+          blankColor: '#777',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+        {
+          blank: true,
+          blankColor: '#777',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+        {
+          blank: true,
+          blankColor: '#777',
+          width: 75,
+          height: 75,
+          class: 'm1',
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
 <template>
   <div>
-    <b
-      ><h2 class="text-primary" :class="$style.title">
+    <div>
+      <h2 class="text-primary" :class="$style.title">
         PAINEL DE CADASTRO - LOCAL
-      </h2></b
-    >
+      </h2>
+    </div>
     <div class="border border-primary" :class="$style.border">
       <b-form @submit="onSubmit">
-        <b-row>
+        <b-row :class="$style.row">
           <b-col>
             <b-form-group
               id="name"
@@ -59,6 +113,7 @@ export default {
             >
               <b-form-input
                 id="name"
+                :class="$style.textInput"
                 v-model="form.name"
                 type="text"
                 placeholder="Novo Local"
@@ -74,6 +129,7 @@ export default {
             >
               <b-form-textarea
                 id="description"
+                :class="$style.textArea"
                 v-model="form.description"
                 placeholder="Descrição única do local"
                 rows="3"
@@ -90,6 +146,7 @@ export default {
               <b-form-input
                 id="contact"
                 v-model="form.contact"
+                :class="$style.textInput"
                 type="text"
                 placeholder="+55 (51) 99999-9999"
                 required
@@ -104,6 +161,7 @@ export default {
             >
               <b-form-textarea
                 id="schedule"
+                :class="$style.textArea"
                 v-model="form.schedule"
                 placeholder="Horários"
                 rows="3"
@@ -114,7 +172,20 @@ export default {
             <b-table striped hover :items="items"></b-table>
           </b-col>
           <b-col>
-            Segunda coluna
+            <b-form-group
+              id="gallery"
+              label="Fotos"
+              label-for="Fotos"
+              description=""
+            >
+<!--              <b-row  v-for="image in Math.ceil(images.length / 4)">-->
+<!--                <b-img-->
+<!--                  v-for="image in images.slice((image - 1) * 4, image * 4)"-->
+<!--                  v-bind="image"-->
+<!--                  rounded alt="images">-->
+<!--                </b-img>-->
+<!--              </b-row>-->
+            </b-form-group>
           </b-col>
         </b-row>
 
@@ -128,14 +199,38 @@ export default {
 .title {
   font-family: "BigShoulders";
   font-size: 3rem;
-  margin-left: 2rem;
+  margin-left: 8rem;
+  margin-top: 4rem;
+  font-weight: 900;
 }
+
+.textInput {
+  background-color: #F8F9FA;
+  border-radius: 0.25rem;
+  border: aliceblue;
+}
+
+.textArea {
+  background-color: #F8F9FA;
+  border-radius: 0.25rem;
+  border: aliceblue;
+  overflow-y: hidden !important;
+}
+
 .border {
-  margin: 2rem;
+  margin: 1rem 8rem;
+  border-radius: 0.75rem;
+  border-color: #9DCC9A !important;
+}
+
+.row {
+  padding: 1.4rem 1.75rem;
 }
 
 label {
   font-family: "BigShoulders";
-  font-size: 2rem;
+  font-size: 1.75rem;
+  font-weight: 600;
+  color: #024053
 }
 </style>
