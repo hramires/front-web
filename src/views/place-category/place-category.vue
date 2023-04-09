8002bc96178c33813e17f0c0dc2b90f321966cc6
Leo <leonardo.soares99@edu.pucrs.br>
2023-04-09 01:26:30 -0300

    Ajustes da tela place-category e listagem de categorias - Leonardo Vargas Soares

--- src/views/place-category/place-category.vue
+++ src/views/place-category/place-category.vue
@@ -6,7 +6,8 @@
       </h2>
     </div>
     <div class="border border-primary" :class="$style.border">
-      <div class="card-header bg-dark-green">
+      <div class="card-header text-white bg-secondary mb-3">
+        <!-- <div class="card-header bg-dark-green" style="background-color: #00AA9E;"> -->
         <h2 class="mb-0">Cadastro de Nova Categoria</h2>
       </div>
       <div class="card-body">
@@ -19,36 +20,38 @@
 
           <b-row :class="$style.buttons">
             <b-col md="6" offset-md="10">
-              <b-button type="button" variant="primary" @click="addCategory">Cadastrar</b-button>
+              <b-button type="button" variant="primary" @click.prevent="addCategory">Cadastrar</b-button>
               <b-button type="reset" variant="danger">Excluir</b-button>
             </b-col>
           </b-row>
         </form>
       </div>
+      <div>
+        <table class="table mt-5" striped hover>
+          <thead>
+            <tr>
+              <th class="text-dark-green">#Id</th>
+              <th class="text-dark-green">Categoria</th>
+              <th class="text-dark-green">Ação</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(category, index) in categories" :key="category.id"
+              :class="{ 'table-secondary': index % 2 == 0, 'table-light': index % 2 != 0 }">
+              <td>{{ category.id }}</td>
+              <td>{{ category.name }}</td>
+              <td>
+                <button type="button" class="btn btn-danger" @click="deleteCategory(index)">X</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th class="text-dark-green">#Id</th>
-          <th class="text-dark-green">Categoria</th>
-          <th class="text-dark-green">Ação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(category, index) in categories" :key="index"
-          :class="{ 'table-success': index % 2 === 0, 'table-light': index % 2 !== 0 }">
-          <td>{{ lastCategoryId + index }}</td>
-          <td>{{ category }}</td>
-          <td><button type="button" :class="$style.buttons" @click="deleteCategory(index)">X</button></td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
   
 <script>
-
 import {
   BForm, BFormGroup, BFormInput, BButton,
 } from 'bootstrap-vue';
@@ -83,26 +86,18 @@ export default {
   },
   methods: {
     addCategory() {
-      if (this.newCategory) {
-        this.categories.push(this.newCategory);
-        this.newCategory = '';
-        this.lastCategoryId += 1;
-      }
+      const id = this.categories.length > 0 ? this.categories[this.categories.length - 1].id + 1 : 1;
+      this.categories.push({ id, name: this.newCategory });
+      this.newCategory = '';
     },
+
     deleteCategory(index) {
       this.categories.splice(index, 1);
-      this.lastCategoryId = this.lastCategoryId - 1;
     }
   }
 }
 </script>
-  
-<style>
-.table-striped>tbody>tr:nth-child(2n+1)>td,
-.table-striped>tbody>tr:nth-child(2n+1)>th {
-  background-color: #9DCC9A;
-}
-</style>
+
 <style type="scss" module>
 .title {
   font-family: "BigShoulders";
@@ -117,31 +112,10 @@ export default {
   margin-bottom: 0.75em;
 }
 
-.table {
-  color: #152536;
-}
-
-.textInput {
-  background-color: #F8F9FA;
-  border-radius: 0.25rem;
-  border: aliceblue;
-}
-
-.textArea {
-  background-color: #F8F9FA;
-  border-radius: 0.25rem;
-  border: aliceblue;
-  overflow-y: hidden !important;
-}
-
 .border {
   margin: 1rem 8rem;
   border-radius: 0.75rem;
   border-color: #9DCC9A !important;
-}
-
-.row {
-  padding: 1.4rem 1.75rem;
 }
 
 label {
@@ -152,5 +126,3 @@ label {
   line-height: 2rem;
 }
 </style>
-
-
