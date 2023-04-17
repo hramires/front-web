fe5e9d39272ab939b5f096008de2ef9e4312c81d
Sofia Carriço Arend <sofia.arend@sap.com>
2023-04-17 11:49:31 -0300

    componentes genericos para tela de listagem

---
+++ src/components/add-button/add-button.vue
@@ -0,0 +1,16 @@
+<script>
+import { BButton } from 'bootstrap-vue';
+
+export default {
+  components: { BButton },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<template>
+  <b-button class="floatButton" @click="onClick">+</b-button>
+</template>
