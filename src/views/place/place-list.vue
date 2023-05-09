7eb7633e44371e2d0ceee1eb02b6e0de88e487c0
Douglas <dodosobreira@gmail.com>
2023-05-09 20:30:38 -0300

    Adicionando componente de filtro por locais

--- src/views/place/place-list.vue
+++ src/views/place/place-list.vue
@@ -3,6 +3,7 @@ import { mapActions, mapState } from 'vuex';
 import { BSpinner } from 'bootstrap-vue';
 import CustomCard from '@components/custom-card';
 import AddButton from '@components/add-button';
+import CustomFilter from '@components/custom-filter';
 
 export default {
   page: {
@@ -11,14 +12,37 @@ export default {
   data() {
     return {
       isLoading: false,
+      filters: [],
+      selectedFilters: {},
     };
   },
-  components: { BSpinner, CustomCard, AddButton },
+  components: {
+    BSpinner, CustomCard, AddButton, CustomFilter,
+  },
   created() {
     this.fetchPlaces();
   },
   computed: {
     ...mapState('places', ['places']),
+    filteredPlaces() {
+      const filterKeys = Object.keys(this.selectedFilters);
+
+      if (filterKeys.length === 0) {
+        return this.places;
+      }
+
+      return this.places.filter((place) => filterKeys.every((key) => {
+        const selectedValues = this.selectedFilters[key];
+        if (!selectedValues || selectedValues.length === 0) {
+          return true;
+        }
+
+        const filter = this.filters.find((f) => f.name === key);
+        const placeValue = place[filter.objectKey];
+
+        return selectedValues.includes(placeValue);
+      }));
+    },
   },
   methods: {
     ...mapActions('places', ['fetchAllPlaces']),
@@ -26,6 +50,7 @@ export default {
       this.isLoading = true;
       await this.fetchAllPlaces();
       if (this.places) {
+        this.createFilters();
         this.isLoading = false;
       } else {
         this.$bvToast.toast('Erro ao buscar locais', {
@@ -41,6 +66,50 @@ export default {
     onClickCreate() {
       this.$router.push({ name: 'cadastrar-local' });
     },
+    updateSelectedFilters(newSelectedFilters) {
+      this.selectedFilters = newSelectedFilters;
+    },
+    createFilters() {
+      const propertyNames = [
+        { key: 'name', name: 'Nomes' },
+        { key: 'description', name: 'Descrição' },
+      ];
+
+      const excludeProperties = [
+        'id',
+        'region_id',
+        'photo_id',
+        'openingHour',
+        'contact',
+        'latitude',
+        'longitude',
+        'createdAt',
+        'updatedAt',
+        'appointment',
+      ];
+
+      const filters = [];
+      this.places.forEach((place) => {
+        Object.keys(place).forEach((key) => {
+          if (excludeProperties.includes(key)) {
+            return;
+          }
+
+          const propertyName = propertyNames.find((property) => property.key === key);
+          const displayName = propertyName ? propertyName.name : key;
+
+          const filter = filters.find((filter) => filter.objectKey === key);
+          if (filter) {
+            if (!filter.values.includes(place[key])) {
+              filter.values.push(place[key]);
+            }
+          } else {
+            filters.push({ name: displayName, values: [place[key]], objectKey: key });
+          }
+        });
+      });
+      this.filters = filters;
+    },
   },
 };
 </script>
@@ -48,10 +117,14 @@ export default {
 <template>
   <div>
     <h1> LOCAIS CADASTRADOS </h1>
+    <custom-filter
+      :filters="filters"
+      @update:selectedFilters="updateSelectedFilters"
+    />
     <b-container class="mw-100">
       <b-row v-if="!isLoading">
         <custom-card
-          v-for="place in places"
+          v-for="place in filteredPlaces"
           :key="place.id"
           :item="place"
           @click="onClickCard(place.id)"
@@ -64,3 +137,5 @@ export default {
     <add-button @click="onClickCreate"/>
   </div>
 </template>
+
+
